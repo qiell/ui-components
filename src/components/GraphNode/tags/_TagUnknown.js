dca5be8fe81e12e1498abf9d35ea933d7fe08b54
Akash Srivastava <akashsrivastava4927@gmail.com>
2018-11-26 16:27:57 +0530

    Add pending tag icon and update CR shapes (#1)
    Signed-off-by: Akash Srivastava <akashsrivastava4927@gmail.com>

--- src/components/GraphNode/tags/_TagUnknown.js
+++ src/components/GraphNode/tags/_TagUnknown.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class TagUnknown extends React.Component {
   render() {
     return (
-      <g transform="translate(6, -5) scale(0.75)">
+      <g transform="translate(1, -8) scale(0.75)">
         <ellipse stroke="#000" ry="14.25" rx="14.74999" id="svg_2" cy="26.74999" cx="25.74998" strokeOpacity="null" strokeWidth="0" fill="#000000"/>
         <path stroke="#000000" id="svg_1" d="m27.82639,39.53438l-5.28719,0l0,-4.01632l5.28719,0l0,4.01632zm0,-8.30474l0,1.32736l-5.28719,0l0,-1.63548c0,-4.93525 7.09024,-5.71843 7.09024,-9.22609c0,-1.59942 -1.80533,-2.82462 -4.16878,-2.82462c-2.4491,0 -4.59717,1.42944 -4.59717,1.42944l-3.01015,-2.96284c0,0 2.96695,-2.45108 8.08044,-2.45108c4.8573,0 9.3673,2.38264 9.3673,6.39954c0.00227,5.61933 -7.47469,6.26737 -7.47469,9.94378l0,0z" strokeWidth="0" fill="#ffffff"/>
       </g>

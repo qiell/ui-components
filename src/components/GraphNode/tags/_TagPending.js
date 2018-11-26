d5d4bc940d018d5218ed71383353f9f7defdd84b
Akash Srivastava <akashsrivastava4927@gmail.com>
2018-11-26 16:27:57 +0530

    Add pending tag icon and update CR shapes (#1)
    Signed-off-by: Akash Srivastava <akashsrivastava4927@gmail.com>

---
+++ src/components/GraphNode/tags/_TagPending.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class TagPending extends React.Component {
+  render() {
+    return (
+      <g transform="translate(1, -7) scale(0.75)">
+        <ellipse stroke="#000" ry="13.92981" rx="13.68107" id="svg_1" cy="26.36713" cx="25.1234" stroke-width="1.5" fill="#000000"/>
+        <ellipse stroke="#000" ry="3.82323" rx="3.62773" id="svg_3" cy="26.49341" cx="15.98214" stroke-width="0" fill="#ffffff"/>
+        <ellipse stroke="#000" ry="3.82323" rx="3.62773" id="svg_8" cy="26.43038" cx="24.65324" stroke-width="0" fill="#ffffff"/>
+        <ellipse stroke="#000" ry="3.82323" rx="3.62773" id="svg_9" cy="26.30432" cx="33.64549" stroke-width="0" fill="#ffffff"/>
+      </g>
+    );
+  }
+}
